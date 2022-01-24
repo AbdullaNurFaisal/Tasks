6234f6b3a8f2a8b7236c4b35e5cd12fc43b76571
krish0ck <abdulla.nur.faisal66@gmail.com>
2022-01-24 14:48:26 +0600

    new commit

---
+++ Task 2/js/script.js
@@ -0,0 +1,5 @@
+const url = "https://gorest.co.in/public/v1/users?access_token=d7c01847de4c083cb154e9a533294301e9f05f93dbae7d589e42ece63226c0a3";
+fetch(url)
+    .then(res => res.json())
+    .then(json => console.log(json))    
+    .catch(err => console.log(err));
